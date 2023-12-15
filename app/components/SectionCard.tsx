6235e76568af3bac8c6ba4a225efcabced830517
Rashaun Warner <rashaunwarner@yahoo.com>
2023-12-15 14:17:43 -0500

    created and implemented SectionCard container component

---
+++ app/components/SectionCard.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+
+export interface SectionCardProps {
+  title: string;
+  children: ReactNode;
+}
+
+const SectionCard: React.FC<SectionCardProps> = ({ title, children }) => {
+  return (
+    <section className="container mx-auto my-8 px-4 sm:px-6 lg:px-8">
+      <h2 className="text-3xl font-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-1 gap-4 bg-white dark:bg-gray-700 rounded-lg shadow-md p-6">
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default SectionCard;
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,19 +1,17 @@
 import React from "react";
+import SectionCard from "./SectionCard";
 
 export default function Skills() {
   const skillsData = ["HTML", "CSS", "React", "React Native", "Django"];
   return (
-    <section id="skills" className="flex flex-col p-4 w-5/6">
-      <h2 className="text-3xl font-bold mb-4">My skills</h2>
-      <div>
-        <ul className="flex flex-wrap justify-center text-lg">
-          {skillsData.map((skill, index) => (
-            <li key={index} className="p-2 m-2 dark:bg-gray-400 rounded-md ">
-              {skill}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
+    <SectionCard title="My Skills">
+      <ul className="flex flex-wrap justify-center text-lg">
+        {skillsData.map((skill, index) => (
+          <li key={index} className="p-2 m-2 dark:bg-gray-400 rounded-md ">
+            {skill}
+          </li>
+        ))}
+      </ul>
+    </SectionCard>
   );
 }
