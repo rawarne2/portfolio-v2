322829348cf77da6c6af86665fa8b3f77625038a
Rashaun Warner <rashaunwarner@yahoo.com>
2024-02-01 15:47:05 -0500

    added project pics and improved style for mobile

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,56 +3,59 @@
 import React from "react";
 import SectionCard from "./SectionCard";
 import { IProject } from "../lib/data";
+import Image from "next/image";
+import { SocialIcon } from "react-social-icons";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import Link from "next/link";
 
 export interface IProjectProps {
   projects: IProject[];
 }
-// make a button - Demo - to show demos if there is one
+// style={{ height: 25, width: 25 }} for icon
 export const Projects: React.FC<IProjectProps> = ({ projects }) => {
   return (
     <SectionCard title="Projects" id="projects">
-      <div className="flex flex-col justify-center">
-        {projects.map((project) => (
-          <div key={project.id}>
-            <ul className="w-full">
-              <li
-                key={project.id}
-                className="p-4 m-4 dark:bg-gray-400 rounded-md"
-              >
-                <h3>
-                  <strong>{project.title}</strong>
-                </h3>
-                <p>{project.description}</p>
-                <p>
-                  <strong>Technologies: </strong>
-                  {project.technologies.join(", ")}
-                </p>
-                <p>
-                  <strong>Github:</strong>{" "}
-                  <a
-                    href={project.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {project.github}
-                  </a>
-                </p>
-                <p>
-                  <strong>Live Demo:</strong>{" "}
-                  <a
-                    href={project.demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    CLICK HERE
-                    {/* {project.demo} */}
-                  </a>
-                </p>
-              </li>
+      {projects.map((project) => (
+        <div
+          key={project.id}
+          className="flex flex-col md:flex-row m-2 md:m-6 mt-4 mb-4 border-4 border-white rounded-md p-4 justify-between"
+        >
+          <div className="flex-col md:flex-[3]">
+            <span className="flex flex-row items-center mb-2 justify-between">
+              <h3 className="text-wrap text-xl font-bold">{project.title}</h3>
+              <span className="flex flex-row justify-between items-center">
+                <SocialIcon
+                  className="rounded-full border-[0.15rem] border-white"
+                  url={project.github}
+                  title="github"
+                  style={{ height: 35, width: 35 }}
+                />
+                {project.link && (
+                  <Link href={project.link}>
+                    <FaExternalLinkAlt
+                      style={{ height: 25, width: 25, marginLeft: 12 }}
+                    />
+                  </Link>
+                )}
+              </span>
+            </span>
+            <p>{project.description}</p>
+            <ul className="flex flex-wrap mt-2">
+              {project.technologies.map((item, i) => (
+                <li
+                  key={i}
+                  className="p-1 m-1 dark:bg-gray-500 rounded-md border-[0.15rem] border-white max-w-fit"
+                >
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
-        ))}
-      </div>
+          <div className="mt-4 md:mt-0 md:flex-1 md:ml-4">
+            <Image src={project.image} alt="Rashaun" className="w-full h-max" />
+          </div>
+        </div>
+      ))}
     </SectionCard>
   );
 };
