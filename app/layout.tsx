b1efe51f67856aa4af5dad3166eca99e2698bb02
Rashaun Warner <rashaunwarner@yahoo.com>
2026-03-17 15:44:49 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 import './styles/globals.css';
 import data from '../public/data.json';
 
@@ -21,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
